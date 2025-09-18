7dcaa3261462a63acef8e957489738c110a00562
Nicholas Furtado <nicholasfurtad5@gmail.com>
2025-09-18 12:01:57 -0400

    Updates to java file

--- SmartHomeTest.java
+++ SmartHomeTest.java
@@ -1,6 +1,83 @@
+interface smartLock 
+{
+    void batteryConsumption();
+    void specs();
+}
+
+//Place holder for lightbulb interface  
+
+//Brand A products
+class BrandALock implements smartLock 
+{
+    @Override
+    public void batteryConsumption() 
+    {
+
+    }
+
+    @Override
+    public void specs() 
+    {
+       
+    }
+}
+
+//Place holder for Brand A lightbulb
+
+//Brand B products
+class BrandBLock implements smartLock 
+{
+    @Override
+    public void batteryConsumption() 
+    {
+    
+    }
+
+    @Override
+    public void specs() 
+    {
+       
+    }
+}
+
+//Place holder for Brand B lightbulb
+
+//Abstract Factory Interface
+interface SmartHomeFactory 
+{
+    smartLock createLock();
+    //Place holder for lightbulb creation method
+}   
+//Factory for Brand A
+class BrandAFactory implements SmartHomeFactory 
+{
+    @Override
+    public smartLock createLock() 
+    {
+        return new BrandALock();
+    }
+
+    //Place holder for lightbulb creation method
+}
+
+//Factory for Brand B
+class BrandBFactory implements SmartHomeFactory 
+{
+    @Override
+    public smartLock createLock() 
+    {
+        return new BrandBLock();
+    }
+
+    //Place holder for lightbulb creation method
+}
+
+//Test Driver for Smart Home System
 public class SmartHomeTest {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Place holder for lock
+        
+        //Place holder for lightbulb 
     }
     
 }
